3fcc71ff41fe284c97ab559c624c0ea4944e0bee
smallcampus <smallcampustin@gmail.com>
2014-01-06 19:09:42 +0800

    delete a file

--- QuickPointer/src/com/example/QuickPointer/net/UDPClient.java
+++ QuickPointer/src/com/example/QuickPointer/net/UDPClient.java
@@ -1,7 +1,6 @@
 package com.example.QuickPointer.net;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
